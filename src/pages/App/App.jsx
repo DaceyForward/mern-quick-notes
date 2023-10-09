d676a756f43644c121f1d114c6e20185074fdaa9
daceyforward <dacey.loving@gmail.com>
2023-10-08 22:24:40 -0400

    user can see all notes page, add note field and button, nav bar with name, signup/sign-in/logout

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,12 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import NewNote from '../NewNote/NewNote';
+import MyNotes from '../MyNotes/MyNotes';
 import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
+  const [notes, setNotes] = useState()
+  // const [showNotes, setShowNotes] = useState(true);
+
+  function addNote(note) {
+    // Replace state, don't mutate it
+    setNotes([...note]);
+  }
 
   return (
     <main className="App">
@@ -17,8 +24,10 @@ export default function App() {
             <NavBar user={user} setUser={setUser} />
             <Routes>
               {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
+              <Route path="/notes/new" element={<NewNote />} />
+              <Route path="/notes" element={<MyNotes addNote={addNote} 
+              // {new Date(note.timestamps).toLocaleDateString()}
+               />} />
             </Routes>
           </>
           :
